19d163a1d18a71a40616362bba3137772793ee9b
Gerardo Suarez Martinez <gerasuarezm27@gmail.com>
2021-10-22 19:27:55 -0500

    added

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function (eleventyConfig) {
+    // Copy static assets over to _site directory.
+    eleventyConfig.addPassthroughCopy('assets/css');
+    eleventyConfig.addPassthroughCopy('assets/img');
+    eleventyConfig.addPassthroughCopy('assets/js');
+    // Return configuration object.
+    return {};
+};
